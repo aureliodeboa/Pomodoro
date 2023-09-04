aab229afbf26c96aa65fc3cde259b18630ac324f
aureliodeboa <aurelio74123@gmail.com>
2023-09-04 14:53:10 -0300

    feature: modal change timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,29 @@ let progressBarEl = document.querySelector('.progress-bar');
 let pomodoroBtn = document.querySelector('.area-bar span:nth-child(1)');
 let shortBtn = document.querySelector('.area-bar span:nth-child(2)');
 let longBtn = document.querySelector('.area-bar span:nth-child(3)');
+let configBtn = document.querySelector('.config');
+let modal= document.querySelector('.modal');
+let closeBtn = document.querySelector('.close');
+let apllyBtn = document.querySelector('.button');
+let pomodoroTimeEl = document.querySelector("#pomodoro-time");
+let shortTimeEl = document.querySelector("#short-time");
+let longTimeEl = document.querySelector("#long-time");
+
 
 let intervalTimer;
-let minutesTimer= 25;
+let minutesTimer= pomodoroTimeEl.value;
 let secondsTimer= 0;
 let isPause= true;
 let shortqt=0;
 let setbar=0;
+let progressPerSecond=0;
 
-let progressPerSecond= 360/(minutesTimer*60); //calcula quanto degraus a progress bar deve se apagada de acordo com o tempo esbelecido
+function calculateProgressBar(minutesTimer){
+    progressPerSecond= 360/(minutesTimer*60); //calcula quanto graus a progress bar deve se apagada de acordo com o tempo esbelecido
+    setbar=progressPerSecond;
+    console.log(minutesTimer);
+}
+
 
 function formateInTime(min,seg){
     
@@ -55,21 +69,23 @@ function startTimer (){
 
 function time(){
     //make time elelment
-    if(secondsTimer === 0){
-        secondsTimer=59;
-        minutesTimer--;
+    if(secondsTimer === 0 && minutesTimer===0){
+       
+        shortqt++; //contabilizar os short break
+        minutesTimer= pomodoroTimeEl.value; //reinicia o tempo
+        setbar=0; //zera a progressbar
+        short();
+        
     }
     else
     {
-        if(minutesTimer === 0){
-            shortqt++; //contabilizar os short break
-            short();
-            minutesTimer= 25; //reinicia o tempo
-            setbar=0; //zera a progressbar
+        if(secondsTimer===0){
+            secondsTimer=59;
+            minutesTimer--;
+
         }
-        else{
-            secondsTimer--;
-        }
+
+        secondsTimer--;
     }
     progressBar();
     timerEl.textContent=formateInTime(minutesTimer,secondsTimer);
@@ -85,28 +101,28 @@ function short(){
         tooglePause();
     }
 
-    minutesTimer=5;
+    minutesTimer=shortTimeEl.value;
     secondsTimer=0;
-
-    timerEl.textContent=formateInTime(minutesTimer,secondsTimer)
-
+    calculateProgressBar(minutesTimer); //calcula a progress bar com o timer do short
+    timerEl.textContent=formateInTime(minutesTimer,secondsTimer)//atualiza a tela com o valor do short
 
 
 }
 
 function pomodoro(){
-    pomodoroBtn.classList.add('active-bar');
     longBtn.classList.remove('active-bar');
     shortBtn.classList.remove('active-bar');
+    pomodoroBtn.classList.add('active-bar');
+   
 
     if(!isPause)
     {
         tooglePause();
     }
 
-    minutesTimer=25;
+    minutesTimer=pomodoroTimeEl.value;
     secondsTimer=0;
-
+    calculateProgressBar(minutesTimer); //calcula a progress bar com o timer do pomodoro
     timerEl.textContent=formateInTime(minutesTimer,secondsTimer)
 
 
@@ -115,26 +131,68 @@ function pomodoro(){
 
 function long(){
     pomodoroBtn.classList.remove('active-bar');
-    longBtn.classList.add('active-bar');
     shortBtn.classList.remove('active-bar');
+    longBtn.classList.add('active-bar');
 
     if(!isPause)
     {
         tooglePause();
     }
 
-    minutesTimer=10;
+    minutesTimer=longTimeEl.value;
     secondsTimer=0;
-
+    calculateProgressBar(minutesTimer); //calcula a progress bar com o timer do long
     timerEl.textContent=formateInTime(minutesTimer,secondsTimer)
 
 
 
 }
 
+function openModal(){
+    modal.classList.add('open');
+
+}
+
+function closeModal(){
+    modal.classList.remove('open');
+}
+
+function reloadTimer()
+{
+    if(pomodoroBtn.classList.contains('active-bar')){
+        pomodoro();
+    }else {
+        if(shortBtn.classList.contains('active-bar')){
+            short();
+        }
+        else
+        {
+            if(longBtn.classList.contains('active-bar')){
+                long();
+            }
+        }
+    }
+
+}
+
+function apllyModal(){
+
+    timerEl.textContent = formateInTime(minutesTimer,secondsTimer);
+    calculateProgressBar(minutesTimer);
+    reloadTimer();
+    closeModal();
+}
+
+
 pomodoroBtn.addEventListener('click',pomodoro);
 shortBtn.addEventListener('click',short);
 longBtn.addEventListener('click',long);
+configBtn.addEventListener('click',openModal);
+closeBtn.addEventListener('click',closeModal);
+apllyBtn.addEventListener('click',apllyModal);
+//modal.addEventListener('click',closeModal);
+
+
 
 clockEl.addEventListener('click',startTimer);
 
